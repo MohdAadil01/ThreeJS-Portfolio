8e47b00237dfd81529f3f5ce7e05a4b6858a6635
MohdAadil01 <mohdaadil4240@gmail.com>
2024-03-09 23:00:05 +0530

    Complete App

---
+++ src/app/data.js
@@ -0,0 +1,64 @@
+export const projectsData = [
+  {
+    id: 1,
+    name: "Creative Crafter",
+    description:
+      "An intuitive online code editor empowering developers to bring ideas to life with ease.",
+    date: "2024-02-15",
+    demoLink: "https://code-craft-rho.vercel.app",
+  },
+  {
+    id: 2,
+    name: "Chatify",
+    description:
+      "Seamlessly connect with friends and colleagues through our intuitive online chat app.",
+    date: "2024-03-07",
+    demoLink: "https://github.com/MohdAadil01/Chatify",
+  },
+  {
+    id: 3,
+    name: "Admin Panel",
+    description:
+      "Seamlessly manage data and monitor operations with our intuitive online dashboard. Streamlined interface for effortless oversight anytime, anywhere",
+    date: "2023-09-10",
+    demoLink: "https://react-js-admin-dashboard-rho.vercel.app",
+  },
+  {
+    id: 4,
+    name: "Tic Tac Toe",
+    description: "A Tic Tack Toe Game",
+    date: "2023-05-30",
+    demoLink: "https://tic-tac-toe-orcin-iota.vercel.app/",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/mohdaadil01",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/mohd-aadil4240",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "X",
+    link: "https://twitter.com/_MOHD_AADIL",
+    icon: "twitter",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "https://drive.google.com/file/d/11tnXo6zM9azUoViKBhzggygDLQp4TitX/view?usp=drive_link",
+    icon: "resume",
+    newTab: true,
+  },
+];
---
+++ src/components/about/index.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import ItemLayout from "./ItemLayout";
+import Link from "next/link";
+
+const AboutDetails = () => {
+  return (
+    <section className="py-20 w-full">
+      <div className="grid grid-cols-12 gap-4 xs:gap-6  md:gap-8 w-full">
+        <ItemLayout
+          className={
+            " col-span-full lg:col-span-8 row-span-2 flex-col items-start"
+          }
+        >
+          <h2 className="  text-xl md:text-2xl text-left w-full capitalize">
+            Architect of Enchantment
+          </h2>
+          <div className="font-light  text-xs sm:text-sm md:text-base   ">
+            Hey there! 👋 I'm MOHD AADIL, a passionate MERN stack developer with
+            a knack for crafting immersive web experiences. With a track record
+            of building dynamic websites like Creative Crafter and Chatify, I
+            thrive on turning ideas into reality through clean, efficient code.
+            Let's create something extraordinary together!
+            <hr className="mt-2 mb-2 border-dashed" />
+            My journey in web development is powered by an array of mystical
+            tools and languages, with JavaScript casting the core of my
+            enchantments. I wield frameworks like React.js and Next.js with
+            precision, crafting seamless portals (websites) that connect realms
+            (users) across the digital universe. Join me as I continue to
+            explore new spells and technologies to shape the future of the web.
+          </div>
+        </ItemLayout>
+
+        <ItemLayout
+          className={" col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            25+ <sub className="font-semibold text-base">Projects</sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            2+{" "}
+            <sub className="font-semibold text-base">years of experience</sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}
+        >
+          <img
+            className="w-full h-auto"
+            src="https://github-readme-stats.vercel.app/api/top-langs?username=mohdaadil01&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false"
+            alt="MOHD AADIL"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
+          <img
+            className="w-full h-auto"
+            src={`https://github-readme-stats.vercel.app/api?username=mohdaadil01&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            alt="MOHD AADIL"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full"}>
+          <img
+            className="w-full h-auto"
+            src={`https://skillicons.dev/icons?i=react,express,mongodb,nodejs,bootstrap,css,docker,figma,firebase,git,github,html,js,jquery,mysql,netlify,nextjs,npm,redux,sass,tailwind,threejs,vercel,vite,vscode,babel`}
+            alt="MOHD AADIL"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+          <img
+            className="w-full h-auto"
+            src="https://github-readme-streak-stats.herokuapp.com?user=MohdAadil01&theme=dark&hide_border=true&type=png&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B"
+            alt="MOHD AADIL"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+          <Link
+            href="https://github.com/codebucks27/Nextjs-contentlayer-blog"
+            target="_blank"
+            className="w-full"
+          >
+            <img
+              className="w-full h-auto"
+              src={`https://github-readme-stats.vercel.app/api/pin/?username=mohdaadil01&repo=CreativeCrafter&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2`}
+              alt="MOHD AADIL"
+              loading="lazy"
+            />
+          </Link>
+        </ItemLayout>
+      </div>
+    </section>
+  );
+};
+
+export default AboutDetails;
